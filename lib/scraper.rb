72afab6b9067a4a48404c0742a005ae6f2e6167a
Daryl Rogony <darylrogony@gmail.com>
2022-06-20 10:19:56 +0300

    commit tree 17f49cc0d774f276c7031dd03730e3a9cd08a64a

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,11 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+ courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+
+ courses.each do |course|
+  puts course.text.strip
+ end
